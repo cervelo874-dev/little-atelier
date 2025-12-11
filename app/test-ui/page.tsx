8ec9e63bf28140a29858d7e5c0902fc522ca087a
cervelo874-dev <cervelo874@gmail.com>
2025-12-11 20:39:19 +0900

    Debug: Add test-ui route to verify deployment pipe

---
+++ app/test-ui/page.tsx
@@ -0,0 +1,11 @@
+export const dynamic = 'force-dynamic'
+
+export default function TestPage() {
+    return (
+        <div className="flex min-h-screen items-center justify-center bg-green-500 text-white font-bold text-4xl p-4 text-center">
+            DEPLOYMENT CONNECTION CONFIRMED
+            <br />
+            (If you see this, the code IS updating)
+        </div>
+    )
+}
